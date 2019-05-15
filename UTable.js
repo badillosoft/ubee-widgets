e22c3a097446e5b3a607fb78e3f5d5596b172f7d
Alan Badillo Salas <badillo.soft@hotmail.com>
2019-05-15 15:46:37 -0500

    UTable

---
+++ UTable.js
@@ -0,0 +1,170 @@
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }
+}
+
+async function installScript(cdn, target = null) {
+    if (!cdn) {
+        console.warn(`Invald script cdn ${cdn}`);
+        return;
+    }
+    const id = window.btoa(cdn);
+    window.scripts = window.scripts || {};
+    if (window.scripts[id]) {
+        return;
+    }
+    target = target || document.body;
+    await new Promise(resolve => {
+        const script = document.createElement("script");
+        script.src = cdn;
+        script.addEventListener("load", () => {
+            window.scripts[id] = script;
+            resolve();
+        });
+
+        target.appendChild(script);
+    });
+}
+
+async function installStyles(cdn, target = null) {
+    if (!cdn) {
+        console.warn(`Invald style cdn ${cdn}`);
+        return;
+    }
+    const id = window.btoa(cdn);
+    window.styles = window.styles || {};
+    if (window.styles[id]) {
+        return;
+    }
+    target = target || document.head;
+    await new Promise(resolve => {
+        const link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = cdn;
+        link.addEventListener("load", () => {
+            window.styles[id] = link;
+            resolve();
+        });
+
+        target.appendChild(link);
+    });
+}
+
+function UTable() {
+    const table = document.createElement("table");
+    const thead = document.createElement("thead");
+    const tbody = document.createElement("tbody");
+
+    // let jtable = null;
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+
+    table.addEventListener("@columns:update", ({ detail: columns }) => {
+        if (!columns || !(columns instanceof Array) || columns.length === 0) {
+            columns = ["Ubee Table"];
+        }
+        table.dataset.columns = JSON.stringify(columns);
+        clearElement(thead);
+        const tr = document.createElement("tr");
+        for (let column of columns) {
+            let th = document.createElement("th");
+            th.innerText = column;
+            th.dataset.columnIndex = column;
+            tr.appendChild(th);
+        }
+        thead.appendChild(tr);
+    });
+
+    table.addEventListener("@rows:add", ({ detail: row }) => {
+        const columns = JSON.parse(table.dataset.columns || "[]");
+        const tr = document.createElement("tr");
+        const index = tbody.children.length;
+        tr.dataset.rowIndex = index;
+        for (let column of columns) {
+            let td = document.createElement("td");
+            td.innerText = row[column] === undefined ? "-" : row[column];
+            td.dataset.rowIndex = index;
+            td.dataset.columnIndex = column;
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    });
+
+    table.addEventListener("@datasource", ({ detail: datasource }) => {
+        clearElement(tbody);
+
+        if (!datasource || !(datasource instanceof Array) || datasource.length === 0) {
+            table.dispatchEvent(new CustomEvent("@columns"));
+            table.dispatchEvent(new CustomEvent("@datasource:add", {
+                detail: [
+                    {
+                        "Ubee Table": "No hay datos"
+                    }
+                ]
+            }));
+            return;
+        }
+
+        table.dispatchEvent(new CustomEvent("@datasource:add", {
+            detail: datasource
+        }));
+    });
+
+    table.addEventListener("@datasource:add", ({ detail: datasource }) => {
+        const columns = datasource.reduce((columns, frame) => {
+            for (let column in frame) {
+                if (columns.indexOf(column) >= 0) {
+                    continue;
+                }
+                columns.push(column);
+            }
+            return columns;
+        }, []);
+        table.dispatchEvent(new CustomEvent("@columns:update", {
+            detail: columns
+        }));
+        for (let frame of datasource) {
+            table.dispatchEvent(new CustomEvent("@rows:add", {
+                detail: frame
+            }));
+        }
+    });
+
+    table.dispatchEvent(new CustomEvent("@datasource"));
+
+    // table.addEventListener("@jquery:datatables", async ({ detail: cdns }) => {
+    //     cdns = cdns || {
+    //         js: [
+    //             // "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js",
+    //             "https://code.jquery.com/jquery-3.4.1.min.js",
+    //             "https://cdn.datatables.net/v/dt/dt-1.10.18/datatables.min.js",
+    //             // "https://cdn.datatables.net/1.10.18/js/dataTables.jqueryui.min.js",
+    //         ],
+    //         css: [
+    //             "https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css",
+    //             // "https://cdn.datatables.net/1.10.18/css/dataTables.jqueryui.min.css",
+    //         ]
+    //     };
+
+    //     for (let cdn of cdns.css) {
+    //         await installStyles(cdn);
+    //     }
+
+    //     for (let cdn of cdns.js) {
+    //         await installScript(cdn);
+    //     }
+
+    //     const columns = JSON.parse(table.dataset.columns || "[]");
+
+    //     console.log(table);
+
+    //     jtable = $(table).DataTable({
+    //         columns,
+    //         data: []
+    //     });
+    // });
+
+    return table;
+}
---
+++ provider.js
@@ -0,0 +1,34 @@
+const socket_io = require("socket.io-client");
+
+const socket = socket_io("http://192.169.200.243:5000");
+
+socket.on("connect", () => {
+    socket.emit("provider@register", { appId: "ubee-test" });
+});
+
+function randomFrame() {
+    const keys = ["A", "B", "C", "D", "E", "F", "G"];
+    const frame = {};
+    for (let i = 0; i < 3; i++) {
+        let key = keys[Math.floor(Math.random() * keys.length)];
+        frame[key] = Number((Math.random() * 100).toFixed(4));
+    }
+    return frame;
+}
+
+const datasource = [];
+
+socket.on("watch", (package) => {
+    const { deviceId, index, data } = package;
+
+    console.log(deviceId.slice(0, 32), index, data);
+    if (index === "table/demo:refresh") {
+        datasource.push(randomFrame());
+        socket.emit("look", {
+            appId: "ubee-test",
+            deviceId,
+            index: "table/demo",
+            data: datasource,
+        });
+    }
+});
